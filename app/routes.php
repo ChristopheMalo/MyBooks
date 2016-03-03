87ef846ee815d03f4f672d20ad2858ede7ce0a2b
ChristopheMalo <christof.malo@gmail.com>
2016-03-03 17:10:33 +0100

    feat: update routes to display detailed book

--- app/routes.php
+++ app/routes.php
@@ -12,3 +12,11 @@ $app->get('/', function() use ($app) {
     $books = $app['dao.book']->findAll();
     return $app['twig']->render('index.html.twig', array('books' => $books));
 })->bind('home');
+
+// Detailed book
+$app->get('/book/{id}', function ($id) use ($app) {
+    $book = $app['dao.book']->find($id);
+    $author = $app['dao.author']->find($id);
+    
+    return $app['twig']->render('book.html.twig', array('book' => $book, 'author' => $author));
+})->bind('book');
