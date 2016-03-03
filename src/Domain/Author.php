fef3e6cdd45e39f48927c30f54df00b4f78db45a
ChristopheMalo <christof.malo@gmail.com>
2016-03-03 16:18:28 +0100

    feat: code class Author

---
+++ src/Domain/Author.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace MyBooks\Domain;
+
+/**
+ * Class representing an Author
+ *
+ * @author      Christophe Malo
+ * @version     1.0.0
+ * @copyright   OpenClassrooms - Baptiste Pesquet
+ */
+class Author {
+    
+    /**
+     * Author id
+     * 
+     * @var int
+     */
+    private $id;
+    
+    /**
+     * Fistname author
+     * 
+     * @var string
+     */
+    private $firstname;
+    
+    /**
+     * Lastname author
+     * 
+     * @var string
+     */
+    private $lastname;
+    
+    
+    // All the Getters
+    /**
+     * Returns author id
+     * 
+     * @return string The author id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+    
+    /**
+     * Returns author firstname
+     * 
+     * @return string The author firstname
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+    
+    /**
+     * Returns author lastname
+     * 
+     * @return string The author lastname
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+    
+    
+    
+    // All the Setters
+    /**
+     * Sets author id
+     * 
+     * @param int $id
+     * @return void
+     */
+    public function setid($id)
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * Sets author firstname
+     * 
+     * @param string $firstname
+     * @return void
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+    
+    /**
+     * Sets author lastname
+     * 
+     * @param string $lastname
+     * @return void
+     */
+    public function setLatsname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+}
