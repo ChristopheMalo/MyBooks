23bc1347143aac553510734bc00844db1b6517cb
ChristopheMalo <christof.malo@gmail.com>
2016-03-04 10:05:09 +0100

    fix: remove code exception

--- src/DAO/AuthorDAO.php
+++ src/DAO/AuthorDAO.php
@@ -18,21 +18,16 @@ class AuthorDAO extends DAO {
      * 
      * @param int $id The author id
      * @return object \MyBooks\Domain\Author The author
-     * @throws \MyBooks\Domain\Author\Exception if no matching author is found
      */
     public function find($id)
     {
         $sql = "SELECT * FROM author WHERE auth_id=?";
         $row = $this->getDb()->fetchAssoc($sql, array($id));
         
-        if ($row)
-        {
-            return $this->buildDomainObject($row);
-        }
-        else
-        {
-            throw new \Exception("No corresponding author for this id " . $id);
-        }
+        // Debug to display data row
+        //var_dump($row); -> Returns array -> an author with data from sql select
+        
+        return $this->buildDomainObject($row);
     }
     
     /**
@@ -45,8 +40,8 @@ class AuthorDAO extends DAO {
     {
         $author = new Author();
         $author->setId($row['auth_id']);
-        $author->setFirstname($row['auth_firstname']);
-        $author->setLastname($row['auth_lastname']);
+        $author->setFirstname($row['auth_first_name']);
+        $author->setLastname($row['auth_last_name']);
         
         return $author;
     }
