3e259d9028f50cad275301d1dbafbc95236f1391
ChristopheMalo <christof.malo@gmail.com>
2016-03-04 09:26:13 +0100

    feat: code a specific route for a detailed book

--- app/routes.php
+++ app/routes.php
@@ -17,6 +17,5 @@ $app->get('/', function() use ($app) {
 $app->get('/book/{id}', function ($id) use ($app) {
     $book = $app['dao.book']->find($id);
     $author = $app['dao.author']->find($id);
-    
     return $app['twig']->render('book.html.twig', array('book' => $book, 'author' => $author));
 })->bind('book');
